49dd16547a1545d5edc194613b92be1881a173cd
Matheus Oliveira <matheus.oliveira@mosaico.com.br>
2021-12-16 08:57:10 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Description
+Just delete old objects from an S3 bucket's folder.
+
+## But why?
+So, I had a motivation to do that, you I don't know =)
+
+## How to run it.
+
+- Before all, run the **npm install** command =)
+- Configure the .env file providing valid values.
+
+### Running
+- npm start
+
+## AWS SDK for NodeJS Docs
+https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html
---
+++ index.js
@@ -0,0 +1,62 @@
+import AWS from 'aws-sdk'
+import sortBy from 'lodash.sortby'
+import findIndex from 'lodash.findindex'
+
+(async () => {
+  const AGE_IN_DAYS = 5 // The number of days to consider an object old.
+
+  const s3 = new AWS.S3()
+
+  try {
+    const s3Objects = await listBucket(s3)
+    let oldObjects = s3Objects.filter(s3Object => !isOldObject(s3Object))
+    if (oldObjects.length > 0) {
+      oldObjects = oldObjects.map(s3Object => ({ Key: s3Object.Key }))
+      oldObjects.slice(0, 1000)
+      await deleteObjects(oldObjects)
+    }
+    console.info(`${s3Objects.length} objects found`)
+    console.info(`${oldObjects.length} old objects deleted`)
+  } catch (error) {
+    console.error(error)
+  }
+
+  async function listBucket () {
+    const params = {
+      Bucket: process.env.BUCKET,
+      Prefix: process.env.PREFIX
+    }
+
+    let s3Objects = []
+    let listObjectsResponse
+    do {
+      listObjectsResponse = await s3.listObjectsV2(params).promise()
+      s3Objects.push(listObjectsResponse.Contents)
+      params.ContinuationToken = listObjectsResponse.NextContinuationToken
+    } while (listObjectsResponse.IsTruncated)
+
+    s3Objects = sortBy(s3Objects.flat(), 'LastModified')
+
+    // Remove the folder itself of the list of the objects.
+    const folderIndex = findIndex(s3Objects, { Key: process.env.PREFIX })
+    s3Objects.splice(folderIndex, 1)
+
+    return s3Objects
+  }
+
+  async function deleteObjects (objects) {
+    const params = {
+      Bucket: process.env.BUCKET,
+      Delete: {
+        Objects: objects,
+        Quiet: false
+      }
+    }
+
+    return s3.deleteObjects(params).promise()
+  }
+
+  function isOldObject (s3Object) {
+    return new Date(s3Object.LastModified).getTime() > (new Date().getTime() - (1000 * 60 * 60 * 24 * AGE_IN_DAYS))
+  }
+})()
